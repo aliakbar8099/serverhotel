ceed0020b6673e8c6db3afe1dbec90e198c23ca7
aliakbar8099 <82612166+aliakbar8099@users.noreply.github.com>
2021-12-13 08:31:31 +0330

    commit 1

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+// server.js
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 4000;
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./DB');
+const mainRoute = require('./mainRoute');
 
-/* 
-    Incase you are using mongodb atlas database uncomment below line
-    and replace "mongoAtlasUri" with your mongodb atlas uri.
-*/
-// mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
 
-app.get('/', (req, res) => {
-  res.send('Hello Wld!')
+// mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {console.log('Database is connected') })
+    .catch( err => { console.log('Can not connect to the database'+ err)});
+
+app.use(cors());
+app.use(express.json())
+
+app.use('/person', mainRoute);
+
+app.use( ( req, res, next) => {
+  res.status( 404);
+  next( new Error( "404 - Not Found"))
+});
+
+app.use( function (err, req, res, next) {
+  if( res.status === 200) res.status( 500)
+  res.json( { msg: err.message})
 })
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+app.listen(PORT, function(){
+  console.log('Server is running on Port:',PORT);
+});
