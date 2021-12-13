190976133b259a813a693e5576ba149ef8b24bc8
aliakbar8099 <82612166+aliakbar8099@users.noreply.github.com>
2021-12-13 08:24:52 +0330

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000
 // mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Wld!')
 })
 
 app.listen(port, () => {
